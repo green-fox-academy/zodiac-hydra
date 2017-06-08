ea50dfb13e8e0c43cb79e032d5b3c938a8ca0aa5
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-08 15:14:30 +0200

    express server created

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
  * Created by Ylwoi on 2017-06-08.
  */
 
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
