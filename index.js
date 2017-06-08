7350ed00cfd6d4d546c95723ff9fdf7f99ce2078
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-08 16:08:37 +0200

    static files use change

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/src'));
+app.use('/public', express.static('public'));
+app.use('/src', express.static('/src'));
 
 app.get('/', function (req, res) {
     //res.render('pages/index');
