196d035c55485eb2a31beb3ab1f12d1b6a5af33b
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-09 10:44:07 +0200

    feat(express)build directory added to static files

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/build'));
 
 app.set('public', __dirname + '/public');
 app.set('src', __dirname + '/src');
+app.set('build', __dirname + '/build');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html')
