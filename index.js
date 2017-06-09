53d1fed6fd3ff39a5ffdb2742eeb5eff99b92e64
Sepler Gábor <sepler.gabor@gmail.com>
2017-06-09 11:51:22 +0200

    express modified

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/src'));
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/src'));
 app.use(express.static(__dirname + '/build'));
+// app.use('/static/', express.static('static'));
 
-app.set('public', __dirname + '/public');
-app.set('src', __dirname + '/src');
+// app.set('public', __dirname + '/public');
+// app.set('src', __dirname + '/src');
 app.set('build', __dirname + '/build');
+app.set('build/static', __dirname + '/build/static');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html')
