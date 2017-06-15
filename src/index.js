7adb9c0254280f610a724408e908c3f343bb3544
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-15 16:48:59 +0200

    App.css imported

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './App.css';
 import Landing from './components/Landing';
 import Nav from './components/Nav';
 import SignUp from './components/SignUp';
