76289c577ee3636d08528045ae50bdad6f7940fe
Horvath Balazs <balazs.horvath989@gmail.com>
2017-06-16 13:48:34 +0200

    css style corrected

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ export class Nav extends React.Component {
       <div className="App-header">
         <h1>Hydra Poker</h1>
         <button className="signupButton">
-          <Link to="/signUp">Sign UP</Link>
+          <Link to="/signUp">SIGN UP</Link>
         </button>
       </div>
 
