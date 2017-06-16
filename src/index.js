051e0cdcda9e4b6ee1245a1a9d7bc8a0e466ffda
Sepler Gábor <sepler.gabor@gmail.com>
2017-06-16 13:42:48 +0200

    conflict resolved again 2

--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from './main';
-
 import registerServiceWorker from './registerServiceWorker';
-
-<<<<<<< HEAD
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comp: Landing
-    }
-  }
-  render() {
-    return (
-      <Router history="">
-        <div className="App">
-          <Nav/>
-          <Route exact path="/" component={this.state.comp}/>
-          <Route path="/signUp" component={SignUp}/>
-          <Route path="/dashboard" component={Dashboard}/>
-        </div>
-      </Router>
-    )
-  }
-}
-=======
-
->>>>>>> testing
 
 ReactDOM.render(
     <App/>
