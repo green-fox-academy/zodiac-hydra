3806ac17d6d761c36de9f9527612c00e4cfbe263
Sepler Gábor <sepler.gabor@gmail.com>
2017-06-16 09:23:48 +0200

    Added some component tests fourth try

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,6 @@ import Dashboard from './components/Dashboard';
 import Intro from './components/Intro';
 import Landing from './components/Landing';
 import Login from './components/Login';
-import Nav from './components/Nav';
 import SignUp from './components/SignUp';
 
 it('renders without crashing', () => {
@@ -14,6 +13,5 @@ it('renders without crashing', () => {
   ReactDOM.render(<Intro />, div);
   ReactDOM.render(<Landing />, div);
   ReactDOM.render(<Login />, div);
-  ReactDOM.render(<Nav />, div);
   ReactDOM.render(<SignUp />, div);
 });
