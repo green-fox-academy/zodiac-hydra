156694c4b9399b22446f1b0bc70a918e8f126d56
Sepler Gábor <sepler.gabor@gmail.com>
2017-06-16 09:02:52 +0200

    Added some component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Ajax from './components/Ajax';
+import Dashboard from './components/Dashboard';
 import Intro from './components/Intro';
+import Landing from './components/Landing';
+import Leaderboard from './components/Leaderboard';
+import Login from './components/Login';
+import Nav from './components/Nav';
+import SignUp from './components/SignUp';
+import Tables from './components/Tables';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  ReactDOM.render(<Ajax />, div);
+  ReactDOM.render(<Dashboard />, div);
   ReactDOM.render(<Intro />, div);
+  ReactDOM.render(<Landing />, div);
+  ReactDOM.render(<Leaderboard />, div);
+  ReactDOM.render(<Login />, div);
+  ReactDOM.render(<Nav />, div);
+  ReactDOM.render(<SignUp />, div);
+  ReactDOM.render(<Tables />, div);
 });
