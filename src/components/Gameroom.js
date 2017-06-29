ee1a26243c2e69eb11ae23e986534494d0ba8bce
Sepler Gábor <sepler.gabor@gmail.com>
2017-06-29 14:37:21 +0200

    Minor game logic and RoundEnd component added

--- src/components/Gameroom.js
+++ src/components/Gameroom.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import Table from './Table';
 import Header from './Header';
-import UserAction from './UserAction';
+import RoundEnd from './RoundEnd';
 
 export class Gameroom extends React.Component {
-    render() {
-        return(
-            <section>
-              <Header/>
-              <Table/>
-              <div className="container">
-                <div className="raiseInput">
-                  <p className="raiseMinus">-</p>
-                  <input ref='raiseInput' type="number" className="raiseInput" placeholder="100"/>
-                  <p className="raisePlus">+</p>
-                </div>
-                <UserAction/>
-              </div>
-            </section>
-        );
-    }
+  render() {
+    return(
+      <section>
+        <Header/>
+        <Table/>
+        <RoundEnd/>
+      </section>
+    );
+  }
 }
 
 export default Gameroom;
