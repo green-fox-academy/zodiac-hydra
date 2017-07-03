beaebfe1dc5bbac38d91eb8802cb5d486cfa51ef
Dora Megyesi <dora.megyesi@gmail.com>
2017-07-03 13:59:12 +0200

    conflicts hopefully solved

--- src/all_reducers.js
+++ src/all_reducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import leaderBoardReducer from './components/Leaderboard/leaderboard_reducer';
 import TablesReducer from './components/Tables/tables_reducer';
+import tableReducer from './components/Table/table_reducer';
 
 const allReducers = combineReducers({
   leaderBoard: leaderBoardReducer,
-  tables: TablesReducer
+  tables: TablesReducer,
+  table: tableReducer
 });
 
 export default allReducers;
