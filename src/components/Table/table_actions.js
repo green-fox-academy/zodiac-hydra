89be2f09fa37544ccf513f59d2a5377e3aa3ae1d
Sepler Gábor <sepler.gabor@gmail.com>
2017-07-14 15:03:37 +0200

    Minor refactors

--- src/components/Table/table_actions.js
+++ src/components/Table/table_actions.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-const root = 'https://equal-koala.glitch.me';
+axios.defaults.headers.common['X-poker-token'] = window.sessionStorage.accessToken;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.headers.put['Content-Type'] = 'application/json';
+axios.defaults.headers.put['Accept'] = 'application/json';
+
+const root = window.sessionStorage.backend;
 
 export const loadTable = (gameData) => {
   return {
